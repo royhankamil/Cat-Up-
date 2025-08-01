00df8f33d7862e83956da3785d39c39b97c481a7
royhankamil <royhan1125@gmail.com>
2025-08-02 05:55:35 +0700

    fix error

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -7,7 +8,7 @@ public class LevelManager : MonoBehaviour
 
     private void Start() 
     {
-        int level = playerprefs.GetInt("level", 1);
+        int level = PlayerPrefs.GetInt("level", 1);
         for (int i = 0; i < buttons.Length; i++)
         {
             if (i < level)
